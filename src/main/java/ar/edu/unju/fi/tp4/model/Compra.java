c811ecb75b2c57a3438a5767128be4e383a5bbd3
3215U <idproinformatica@gmail.com>
2021-05-09 18:06:25 -0300

    Detalle: modificacion model, eliminacion de calculo en set, y agregar constructor de producto. Responsable: Roxana Benicio

--- src/main/java/ar/edu/unju/fi/tp4/model/Compra.java
+++ src/main/java/ar/edu/unju/fi/tp4/model/Compra.java
@@ -15,6 +15,14 @@ public class Compra {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
+	
+	public Compra( Producto producto) {
+		super();
+		
+		this.producto = producto;
+		
+	}
 	/**
 	 * @param id
 	 * @param producto
@@ -28,58 +36,53 @@ public class Compra {
 		this.cantidad = cantidad;
 		this.total = total;
 	}
-	/**
-	 * @return the id
-	 */
+	
+	
 	public int getId() {
 		return id;
 	}
-	/**
-	 * @param id the id to set
-	 */
+
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	/**
-	 * @return the producto
-	 */
+
+
 	public Producto getProducto() {
 		return producto;
 	}
-	/**
-	 * @param producto the producto to set
-	 */
+
+
 	public void setProducto(Producto producto) {
 		this.producto = producto;
 	}
-	/**
-	 * @return the cantidad
-	 */
+
+
 	public int getCantidad() {
 		return cantidad;
 	}
-	/**
-	 * @param cantidad the cantidad to set
-	 */
+
+
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
 	}
-	/**
-	 * @return the total
-	 */
+
+
 	public double getTotal() {
 		return total;
 	}
-	/**
-	 * @param total the total to set
-	 */
+
+
 	public void setTotal(double total) {
 		this.total = total;
 	}
+
+
 	@Override
 	public String toString() {
 		return "Compra [id=" + id + ", producto=" + producto + ", cantidad=" + cantidad + ", total=" + total + "]";
 	}
+
 	
 	
 }
